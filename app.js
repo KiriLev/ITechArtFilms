926feca2911101c042dd8a5f2516d2c405e144e8
KiriLev <klmail98@gmail.com>
2018-03-16 15:52:42 +0300

    changed start script

--- app.js
+++ app.js
@@ -1,15 +1,2 @@
-const mongoose = require('mongoose');
-mongoose.connect(`mongodb+srv://server:${encodeURIComponent('yWwHkBIqAaNmnbll')}@datacluster-iv9to.mongodb.net/test`);
+const server = require('./server/app');
 
-/*
-const connectionString = ``;
-
-console.log(encodeURIComponent('2y4^VvGtSYu45rUd'))
-
-MongoClient.connect(connectionString, function (err, db) {
-    console.log(err);
-    console.log("Connected successfully to server");
-
-    db.close();
-});
-*/
