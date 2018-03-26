9c93b36a460eef961d23fa56a37cfbdae7d74689
KiriLev <klmail98@gmail.com>
2018-03-26 11:24:23 +0300

    added rating

--- client/src/Mainpage/components/FilmCard.jsx
+++ client/src/Mainpage/components/FilmCard.jsx
@@ -6,6 +6,7 @@ import Card, { CardActions, CardContent, CardMedia } from 'material-ui/Card';
 
 import Button from 'material-ui/Button';
 import Typography from 'material-ui/Typography';
+import ReactStars from 'react-stars';
 
 
 const styles = {
@@ -16,10 +17,17 @@ const styles = {
     media: {
         height: 500,
     },
+    rating:{
+        width: "55%",
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-between",
+    }
 };
 
 
-const FilmCard = ({ classes, title, description, picLink }) => {
+const FilmCard = ({ classes, title, description, picLink, rating }) => {
     return (
         <Card className={classes.card}>
             <CardMedia
@@ -31,6 +39,19 @@ const FilmCard = ({ classes, title, description, picLink }) => {
                 <Typography gutterBottom variant="headline" component="h2">
                     {title}
                 </Typography>
+                <div className={classes.rating}>
+                    <ReactStars
+                        count={10}
+                        value={rating}
+                        edit={false}
+                        size={24}
+                    />
+
+                    <Typography variant="headline" component="h2">
+                        {rating}
+                    </Typography>
+                </div>
+
                 <Typography component="p">
                     {description}
                 </Typography>
