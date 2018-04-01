dfc212a590ceebeae1442309f6cff9a680e1e717
KiriLev <klmail98@gmail.com>
2018-04-01 18:01:46 +0300

    fix import of reducers + build

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,8 +1,8 @@
 import { combineReducers } from 'redux';
 import { reducer as formReducer } from 'redux-form';
 
-import { MainpageReducers } from '../Mainpage/reducers'
-import { FilmPageReducers } from '../FilmPage/reducers'
+import MainpageReducers from '../Mainpage/reducers'
+import FilmPageReducers from '../FilmPage/reducers'
 
 
 
